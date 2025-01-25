db68382f6be7fb772c1c28071be786e7f6418960
Rochamukti Rizcanofana <rochamukti@gmail.com>
2025-01-25 19:25:07 +0700

    delete
    delete

---
+++ 01 Containerization and Infrastructure as Code/02 Terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {
+    credentials = "./keys/mycreds.json"
+  project = "terraform-trial-448907"
+  region  = "us-central1"
+}
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-trial-448907-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
